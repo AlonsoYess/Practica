a8e828895ada99b590c883fede448ce050da718e
Alonso Ttica <alonsoticaleon@gmail.com>
2025-06-11 17:42:28 -0500

    Configuracion de Firebase

--- app/src/main/java/com/dta/practica/MainActivity.kt
+++ app/src/main/java/com/dta/practica/MainActivity.kt
@@ -13,10 +13,21 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.dta.practica.ui.theme.PracticaTheme
 
+// Import de Firebase Analytics
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.analytics.FirebaseAnalytics
+
 class MainActivity : ComponentActivity() {
+
+    private lateinit var firebaseAnalytics: FirebaseAnalytics
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        firebaseAnalytics = Firebase.analytics
+        firebaseAnalytics.logEvent("app_opened", null)
         setContent {
             PracticaTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
